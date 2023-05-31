c8b965a7d4cab53c55035da118fd142a155153ae
richardmundyiii <mundy.richard@icloud.com>
2023-05-31 14:30:53 -0700

    add unqualified players

--- src/pages/PlayerStandingsPage/PlayerStandingsPage.jsx
+++ src/pages/PlayerStandingsPage/PlayerStandingsPage.jsx
@@ -5,79 +5,86 @@ import "./PlayerStandingsPage.css";
 
 export default function PlayerStandingsPage({ user, setUser }) {
   const [division, setDivision] = useState("A");
-  const [standings, setStandings] = useState([]);
-  const [sortOrder, setSortOrder] = useState("asc");
-  const [sortBy, setSortBy] = useState("Win %");
+  const [qualifiedPlayers, setQualifiedPlayers] = useState([]);
+  const [unqualifiedPlayers, setUnqualifiedPlayers] = useState([]);
+  const [sortField, setSortField] = useState(null);
+  const [sortDirection, setSortDirection] = useState("asc");
 
   useEffect(() => {
     async function getPlayerStandings() {
       const standings = await playerStandingsApi.getPlayerStandings(division);
-      setStandings(standings);
+      const qualified = [];
+      const unqualified = [];
+
+      standings.forEach((currentPlayer) => {
+        const playerStats = currentPlayer.stats.reduce(
+          (acc, match) => {
+            acc.wins += match.wins;
+            acc.losses += match.losses;
+            acc.cricketHighlights += match.cricketHighlights;
+            acc.zeroOneHighlights += match.zeroOneHighlights;
+            acc.zeroOnePoints += match.zeroOnePoints;
+            return acc;
+          },
+          {
+            wins: 0,
+            losses: 0,
+            cricketHighlights: 0,
+            zeroOneHighlights: 0,
+            zeroOnePoints: 0,
+          }
+        );
+
+        playerStats.winPercentage =
+          (playerStats.wins / (playerStats.wins + playerStats.losses)) * 100;
+
+        const player = {
+          _id: currentPlayer._id,
+          name: currentPlayer.name,
+          ...playerStats,
+        };
+
+        if (playerStats.wins === 0 && playerStats.losses === 0) {
+          return;
+        }
+
+        if (
+          (division === "A" && playerStats.wins + playerStats.losses < 13) ||
+          (division === "B" && playerStats.wins + playerStats.losses < 15)
+        ) {
+          unqualified.push(player);
+        } else {
+          qualified.push(player);
+        }
+      });
+
+      function sortPlayers(a, b) {
+        if (a[sortField] < b[sortField]) {
+          return sortDirection === "asc" ? -1 : 1;
+        }
+        if (a[sortField] > b[sortField]) {
+          return sortDirection === "asc" ? 1 : -1;
+        }
+        return 0;
+      }
+
+      if (sortField) {
+        qualified.sort(sortPlayers);
+        unqualified.sort(sortPlayers);
+      }
+
+      setUnqualifiedPlayers(unqualified);
+      setQualifiedPlayers(qualified);
     }
     getPlayerStandings();
-  }, [division]);
+  }, [division, sortField, sortDirection]);
 
-  const proData = standings
-    .map((currentPlayer) => {
-      const playerStats = currentPlayer.stats.reduce(
-        (acc, match) => {
-          acc.wins += match.wins;
-          acc.losses += match.losses;
-          acc.cricketHighlights += match.cricketHighlights;
-          acc.zeroOneHighlights += match.zeroOneHighlights;
-          acc.zeroOnePoints += match.zeroOnePoints;
-          return acc;
-        },
-        {
-          wins: 0,
-          losses: 0,
-          cricketHighlights: 0,
-          zeroOneHighlights: 0,
-          zeroOnePoints: 0,
-        }
-      );
-      playerStats.winPercentage =
-        (playerStats.wins / (playerStats.wins + playerStats.losses)) * 100;
-
-      return {
-        _id: currentPlayer._id,
-        name: currentPlayer.name,
-        ...playerStats,
-      };
-    })
-    .filter((player) => player.wins !== 0 || player.losses !== 0)
-    .sort((a, b) => {
-      if (sortOrder === "desc") {
-        [a, b] = [b, a];
-      }
-      switch (sortBy) {
-        case "Place":
-          return proData.indexOf(a) - proData.indexOf(b);
-        case "Player":
-          return a.name.localeCompare(b.name);
-        case "Wins":
-          return b.wins - a.wins;
-        case "Losses":
-          return a.losses - b.losses;
-        case "Win %":
-          return b.winPercentage - a.winPercentage;
-        case "Cr. Highlights":
-          return b.cricketHighlights - a.cricketHighlights;
-        case "'01 Highlights":
-          return b.zeroOneHighlights - a.zeroOneHighlights;
-        case "'01 Points":
-          return b.zeroOnePoints - a.zeroOnePoints;
-        default:
-          return 0;
-      }
-    });
-
-  function handleSort(name) {
-    if (sortBy === name) {
-      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  function handleSort(field) {
+    if (sortField === field) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
     } else {
-      setSortOrder("asc");
-      setSortBy(name);
+      setSortField(field);
+      setSortDirection("asc");
     }
   }
 
@@ -94,23 +101,64 @@ export default function PlayerStandingsPage({ user, setUser }) {
             <thead>
               <tr>
                 <th>Place</th>
-                <th>Player</th>
-                <th onClick={() => handleSort("Wins")}>Wins</th>
-                <th onClick={() => handleSort("Losses")}>Losses</th>
-                <th onClick={() => handleSort("Win %")}>Win %</th>
-                <th onClick={() => handleSort("Cr. Highlights")}>
+                <th onClick={() => handleSort("name")}>Player</th>
+                <th onClick={() => handleSort("wins")}>Wins</th>
+                <th onClick={() => handleSort("losses")}>Losses</th>
+                <th onClick={() => handleSort("winPercentage")}>Win %</th>
+                <th onClick={() => handleSort("cricketHighlights")}>
                   Cr. Highlights
                 </th>
-                <th onClick={() => handleSort("'01 Highlights")}>
+                <th onClick={() => handleSort("zeroOneHighlights")}>
                   01 Highlights
                 </th>
-                <th onClick={() => handleSort("'01 Points")}>01 Points</th>
+                <th onClick={() => handleSort("zeroOnePoints")}>01 Points</th>
               </tr>
             </thead>
             <tbody>
-              {proData.map((player, idx) => (
+              {qualifiedPlayers.map((player, idx) => (
                 <tr key={idx}>
                   <td>{idx + 1}</td>
+                  <td>
+                    <Link
+                      to={`/players/${player._id}`}
+                      className="btn btn-success"
+                      style={{ width: "100%" }}
+                    >
+                      {player.name}
+                    </Link>
+                  </td>
+                  <td>{player.wins}</td>
+                  <td>{player.losses}</td>
+                  <td>{player.winPercentage.toFixed(2)}%</td>
+                  <td>{player.cricketHighlights}</td>
+                  <td>{player.zeroOneHighlights}</td>
+                  <td>{player.zeroOnePoints}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="table-responsive mt-4">
+          <h3>Unqualified Players</h3>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th onClick={() => handleSort("name")}>Player</th>
+                <th onClick={() => handleSort("wins")}>Wins</th>
+                <th onClick={() => handleSort("losses")}>Losses</th>
+                <th onClick={() => handleSort("winPercentage")}>Win %</th>
+                <th onClick={() => handleSort("cricketHighlights")}>
+                  Cr. Highlights
+                </th>
+                <th onClick={() => handleSort("zeroOneHighlights")}>
+                  01 Highlights
+                </th>
+                <th onClick={() => handleSort("zeroOnePoints")}>01 Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              {unqualifiedPlayers.map((player, idx) => (
+                <tr key={idx}>
                   <td>
                     <Link
                       to={`/players/${player._id}`}
